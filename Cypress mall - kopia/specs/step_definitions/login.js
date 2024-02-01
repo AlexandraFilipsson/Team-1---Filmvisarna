f2a8c19753fe7f0c4543cf6fb878611011ef8938
SakuyaYae <darkreimu@outlook.com>
2024-02-01 12:11:40 +0100

    Create login.js

---
+++ Cypress mall - kopia/specs/step_definitions/login.js
@@ -0,0 +1,77 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given('that I am on the log in page', () => {
+  cy.visit("/logga-in")
+});
+
+When('I fill the email feald whit wrong@error.example', () => {
+  cy.get("div.form-wrapper div:nth-child(1) input").type("wrong@error.example");
+});
+
+When('I fill the password feald whit Ananas123456789', () => {
+  cy.get("div.form-wrapper div:nth-child(2) input").type("Ananas123456789");
+});
+When('I click on the confirm-button', () => {
+  cy.get("div.Login-btn").click();
+});
+
+Then('I should get an error massage', () => {
+  cy.get("p.server-error").should("be.visible");
+});
+
+/* No duplicate steps, this one already above
+Given('that I am on the log in page', () => {});*/
+
+When('I fill the email feald whit Sakuya.yae@Darkness.ananas', () => {
+  cy.get("div.form-wrapper div:nth-child(1) input").type("Sakuya.yae@Darkness.ananas")
+});
+
+When('I fill the password feald whit errorERR0RerrorERR0R', () => {
+  cy.get("div.form-wrapper div:nth-child(2) input").type("errorERR0RerrorERR0R");
+});
+
+/* No duplicate steps, this one already above
+When('I click on the confirm-button', () => {});*/
+
+/* No duplicate steps, this one already above
+Then('I should get an error massage', () => {});*/
+
+/* No duplicate steps, this one already above
+Given('that I am on the log in page', () => {});*/
+
+/* No duplicate steps, this one already above
+When('I fill the email feald whit Sakuya.yae@Darkness.ananas', () => {});*/
+
+When('I fill the password feald whit nothing in it', () => {
+  cy.get("div.form-wrapper div:nth-child(2) input")
+});
+
+/* No duplicate steps, this one already above
+When('I click on the confirm-button', () => {});*/
+
+/* No duplicate steps, this one already above
+Then('I should get an error massage', () => {});*/
+
+/* No duplicate steps, this one already above
+Given('that I am on the log in page', () => {});*/
+
+/* No duplicate steps, this one already above
+When('I fill the email feald whit Sakuya.yae@Darkness.ananas', () => {});*/
+
+/* No duplicate steps, this one already above
+When('I fill the password feald whit Ananas123456789', () => {});*/
+
+/* No duplicate steps, this one already above
+When('I click on the confirm-button', () => {});*/
+
+Then('I should be loged in', () => {
+  // TODO: implement step
+});
+
+Then('be taken to the register page', () => {
+  // TODO: implement step
+});
+
+Then('be able to see the {string} button', (a) => {
+  // TODO: implement step
+});
