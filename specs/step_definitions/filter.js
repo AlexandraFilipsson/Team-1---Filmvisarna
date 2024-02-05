326b97f62215f4699af614726e533927975c871b
AlexandraFilipsson <alexandrafilipsson@gmail.com>
2024-02-05 15:01:03 +0100

    fixed errors

--- specs/step_definitions/filter.js
+++ specs/step_definitions/filter.js
@@ -2,72 +2,55 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 Given('that I am on the startpage', () => {
   cy.visit('./');
+  cy.viewport(1200, 900)
 });
 
 Given('I have clicked on Filmer', () => {
-  it('Clicks on the "Filmer" link', () => {
-    cy.get('body div header nav ul li:eq(0) a').click();
-
-  });
+  cy.get('header nav a[href="/filmer"]').click();
 });
 
 When('I click on På bio nu', () => {
   cy.log('Attempting to click on På bio nu');
-  cy.contains('På bio nu').click();
+  cy.get('select[name="upcoming"]').select("false");
 });
 
 
 When('I apply the filter', () => {
-  When('I apply the filter', () => {
-    cy.log('Attempting to apply the filter');
-  })
-});
+  cy.log('Attempting to apply the filter');
+})
 
 
 Then('I should be able to see all movies that are currently in cinemas', () => {
-  Then('I should be able to see all movies that are currently in cinemas', () => {
-    // Wait for the movies to be loaded
-    cy.get('.cinema-movies').should('exist').should('be.visible');
-  });
+  cy.get('').should('exist')
 
 });
 
 
 When('I pick a specific date', () => {
-  When('I pick a specific date', () => {
-    // Assuming the input field for the date has a unique identifier or class
-    cy.get('body div main ul li:nth-child(2) input').type('2024-02-14'); // Assuming '2024-02-01' is the desired date format
-  });
-
+  // Assuming the input field for the date has a unique identifier or class
+  cy.get('body div main ul li:nth-child(2) input').type('2024-02-14'); // Assuming '2024-02-01' is the desired date format
 });
 
 When('I apply the date filter', () => {
-  When('I apply the date filter', () => {
-    cy.get('.sectionClass').should('exist');
-  })
-});
+  cy.get('.sectionClass').should('exist');
+})
 
 
 Then('I should be able to see all movies scheduled for that date', () => {
-  Then('I should be able to see all movies scheduled for that date', () => {
-    cy.get('body > div > main > section').should('be.visible');
-  });
+  cy.get('body > div > main > section').should('be.visible');
 });
+
 
 When('I select an age limit option', () => {
-  When('I select an age limit option', () => {
-    cy.get('body > div > main > ul > li:eq(2) > select').select('YourAgeLimitOption');
-  });
+  cy.get('body > div > main > ul > li:eq(2) > select').select('YourAgeLimitOption');
 });
+
 
 When('I apply the age limit filter', () => {
-  When('I apply the age limit filter', () => {
-    cy.get('body > div > main > section').should('exist');
-  });
+  cy.get('body > div > main > ul > li:eq(2) > select').select('YourAgeLimitOption');
 });
 
+
 Then('I should be able to see all movies that meet the selected age limit', () => {
-  Then('I should be able to see all movies that meet the selected age limit', () => {
-    cy.get('body > div > main > section').should('exist');
-  });
+  cy.get('body > div > main > section').should('exist');
 });
